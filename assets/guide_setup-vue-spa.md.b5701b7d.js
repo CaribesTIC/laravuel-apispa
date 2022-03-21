7eef8eae7dec33613a6333e3e892f795e691056f
ejimenez123 <caribestic123@gmail.com>
2022-03-21 18:38:35 -0400

    deploy

---
+++ assets/guide_setup-vue-spa.md.b5701b7d.js
@@ -0,0 +1,9 @@
+import{_ as e,c as a,o as n,d as t}from"./app.318e13ba.js";const m='{"title":"Setup Vue SPA","description":"","frontmatter":{},"headers":[],"relativePath":"guide/setup-vue-spa.md"}',s={},p=t(`<h1 id="setup-vue-spa" tabindex="-1">Setup Vue SPA <a class="header-anchor" href="#setup-vue-spa" aria-hidden="true">#</a></h1><p>It is assumed you have some experience working with VueJS, its router and state management package <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener noreferrer">Pinia</a>. You can install the <a href="https://vitest.dev/" target="_blank" rel="noopener noreferrer">Vite</a> using:</p><div class="language-js"><pre><code><span class="token comment">// with npm</span>
+$ npm install <span class="token operator">-</span><span class="token constant">D</span> vitest
+
+<span class="token comment">// or with yarn</span>
+$ yarn add <span class="token operator">-</span><span class="token constant">D</span> vitest
+
+<span class="token comment">// or with pnpm</span>
+$ pnpm add <span class="token operator">-</span><span class="token constant">D</span> vitest
+</code></pre></div><p>Install the following packages:</p><ul><li><p>Vue Router</p></li><li><p>Pinia</p></li><li><p>Axios</p></li><li><p>Tailwind CSS</p></li></ul>`,5),o=[p];function r(i,l,c,u,d,_){return n(),a("div",null,o)}var k=e(s,[["render",r]]);export{m as __pageData,k as default};
