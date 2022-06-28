da749e46d618ff6c41f211dadeb3e76a3108c694
ejimenez123 <caribestic123@gmail.com>
2022-06-28 14:56:20 -0400

    deploy

---
+++ assets/vue_setup-vue-spa.md.5d0e3506.js
@@ -0,0 +1,9 @@
+import{_ as e,c as a,o as n,d as t}from"./app.7176605b.js";const m='{"title":"Configurar Vue SPA","description":"","frontmatter":{},"headers":[],"relativePath":"vue/setup-vue-spa.md"}',s={},p=t(`<h1 id="configurar-vue-spa" tabindex="-1">Configurar Vue SPA <a class="header-anchor" href="#configurar-vue-spa" aria-hidden="true">#</a></h1><p>Se supone que tiene algo de experiencia trabajando con VueJS, su enrutador y el paquete de administraci\xF3n de estado <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener noreferrer">Pinia</a>. Puede instalar el <a href="https://vitest.dev/" target="_blank" rel="noopener noreferrer">Vite</a> usando:</p><div class="language-js"><pre><code><span class="token comment">// with npm</span>
+$ npm install <span class="token operator">-</span><span class="token constant">D</span> vitest
+
+<span class="token comment">// or with yarn</span>
+$ yarn add <span class="token operator">-</span><span class="token constant">D</span> vitest
+
+<span class="token comment">// or with pnpm</span>
+$ pnpm add <span class="token operator">-</span><span class="token constant">D</span> vitest
+</code></pre></div><p>Instale los siguientes paquetes:</p><ul><li><p>TypeScript</p></li><li><p>Pinia</p></li><li><p>Vue Router</p></li><li><p>Axios</p></li><li><p>Tailwind CSS</p></li><li><p>Vitest</p></li><li><p>Vue Test Utils</p></li><li><p>Vue Testing Library</p></li><li><p>Jest Dom Testing Library</p></li></ul>`,5),i=[p];function r(o,l,c,u,d,_){return n(),a("div",null,i)}var f=e(s,[["render",r]]);export{m as __pageData,f as default};
